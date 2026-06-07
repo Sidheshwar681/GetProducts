2d2d3e1149c0e39e58bbc1971b6adcb5226d7bf2
Sidheshwar Kokalge <kokalgesidheshwar45@gmail.com>
2026-06-07 19:42:23 +0530

    Configure build and deployment for Render service

--- Program.cs
+++ Program.cs
@@ -13,6 +13,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,7 +34,7 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseAuthorization();
-
+app.UseCors("AllowAll");
 app.MapControllers();
 
 app.Run();
